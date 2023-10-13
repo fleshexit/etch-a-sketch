e940fcbc28ecc09e198c2c4e272b382023d56da4
Charlie <chrlie.w@proton.me>
2023-10-13 22:53:54 +0100

    changed event listener from mouse hover to click and hold

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
-
 let colour = 'black';
-let click = true;
+let pressed = false; 
 
-function populateGrid(size){
-
+function populateGrid(size) {
     let grid = document.querySelector('.grid');
     let cells = document.querySelectorAll('.cell');
 
@@ -14,40 +11,44 @@ function populateGrid(size){
 
     let totalCells = size * size;
     for (let i = 0; i < totalCells; i++) {
-
         let cell = document.createElement('div');
-        cell.addEventListener('mouseover', colourCell);
+        cell.addEventListener('mousedown', () => {
+            pressed = true;
+            colourCell.call(cell);
+        });
+        cell.addEventListener('mouseup', () => {
+            pressed = false;
+        });
+        cell.addEventListener('mouseenter', () => {
+            if (pressed) {
+                colourCell.call(cell);
+            }
+        });
         cell.classList.add('cell');
         cell.style.backgroundColor = 'white';
         grid.insertAdjacentElement('beforeend', cell);
     }
 }
 
-function changeSize(input){
+function changeSize(input) {
     populateGrid(input);
 }
 
-function colourCell(){
-    if (click){
-        if (colour == 'rainbow'){
-            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-        } else {
-            this.style.backgroundColor = colour;
-        }
+function colourCell() {
+    if (colour == 'rainbow') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = colour;
     }
-
 }
 
-function changeColour(choice){
-
+function changeColour(choice) {
     colour = choice;
 }
 
-function clearGrid(){
+function clearGrid() {
     let cells = document.querySelectorAll('.cell');
     cells.forEach(cell => cell.style.backgroundColor = 'white');
 }
-
-
 
 populateGrid(16);
